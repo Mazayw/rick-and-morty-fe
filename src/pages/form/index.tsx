811f691753854c484175a21d1bdbbda4add3e267
Mazayw <mazayw@gmail.com>
2022-10-09 14:54:13 +0300

    feat: add email check

--- src/pages/form/index.tsx
+++ src/pages/form/index.tsx
@@ -12,6 +12,7 @@ export default class Form extends PureComponent<
       switcher: boolean | undefined;
       file: FileList | undefined | null;
     };
+    emailError: string | undefined;
   }
 > {
   constructor(props: Record<string, never>) {
@@ -42,6 +43,7 @@ export default class Form extends PureComponent<
       switcher: this.switcher.current?.checked,
       file: this.file.current?.files,
     };
+    this.emailHandler();
     await this.setState({ answer: result });
     /* 
     this.setState({ answer: { ...this.state.answer, date: this.date.current?.value } });
@@ -51,24 +53,29 @@ export default class Form extends PureComponent<
     */
     console.log(this.state);
   }
-  /*
-  emailHandler() {
+
+  async emailHandler() {
     const reg =
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (!reg.test(String(e.target.value).toLowerCase())) {
-      setEmailError('Некорректный E-mail');
-    }
-  }*/
+    if (!reg.test(String(this.input.current?.value).toLowerCase())) {
+      await this.setState({ emailError: 'Некорректный E-mail' });
+    } else await this.setState({ emailError: '' });
+  }
 
   render() {
     return (
       <div className={styles.main}>
         <form onSubmit={this.handleSubmit} className={styles.form}>
+          {this.state && this.state['emailError'] && <h2>{this.state.emailError}</h2>}
           <label>
             E-mail:
-            <input type="text" ref={this.input} className={styles.name} />
+            <input
+              type="text"
+              ref={this.input}
+              className={styles.name}
+              onChange={() => this.setState({ emailError: '' })}
+            />
           </label>
-
           <label>
             Start date:
             <input type="date" defaultValue="2022-01-01" ref={this.date}></input>
@@ -96,7 +103,6 @@ export default class Form extends PureComponent<
             Select a file:
             <input type="file" id="myfile" name="myfile"></input>
           </label>
-
           <input type="submit" value="Submit" />
         </form>
       </div>
