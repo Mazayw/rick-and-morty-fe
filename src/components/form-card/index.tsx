676163eb12a82b71d92783872f18a78022ba5e68
Mazayw <mazayw@gmail.com>
2022-10-21 00:43:05 +0300

    feat: form-card rewrite

--- src/components/form-card/index.tsx
+++ src/components/form-card/index.tsx
@@ -1,53 +1,45 @@
 import styles from './styles.module.scss';
-import React, { Component } from 'react';
+import React from 'react';
 import { ICardForm } from 'components/interfaces';
 
-export default class FormCard extends Component<{
-  cardData: ICardForm;
-}> {
-  constructor(props: { cardData: ICardForm }) {
-    super(props);
-  }
+export default function FormCard({ cardData }: { cardData: ICardForm }) {
+  const { email, date, select, checkbox, switcher, imagePreviewUrl } = cardData;
 
-  render() {
-    const { email, date, select, checkbox, switcher, imagePreviewUrl } = this.props.cardData;
-
-    return (
-      <div className={styles['card-wrapper']} data-testid="card">
-        <div className={styles['image-wrapper']}>
-          {' '}
-          <img
-            src={imagePreviewUrl as string}
-            className={styles.image}
-            alt="Uploaded image"
-            title="Uploaded image"
-          />
-        </div>
-        <div className={styles.text}>
-          <ul className={styles.about}>
-            <li className={styles.description}>
-              <span className={styles.bold}> E-mail: </span>
-              {email}{' '}
-            </li>
-            <li className={styles.description}>
-              <span className={styles.bold}>Date: </span>
-              {date}
-            </li>
-            <li className={styles.description}>
-              <span className={styles.bold}>Charater: </span>
-              {select}
-            </li>
-            <li className={styles.description}>
-              <span className={styles.bold}>Receive news by mail: </span>
-              {checkbox ? 'Yes' : 'No'}
-            </li>
-            <li className={styles.description}>
-              <span className={styles.bold}> Receive advertisements: </span>
-              {switcher ? 'Yes' : 'No'}
-            </li>
-          </ul>
-        </div>
+  return (
+    <div className={styles['card-wrapper']} data-testid="card">
+      <div className={styles['image-wrapper']}>
+        {' '}
+        <img
+          src={imagePreviewUrl as string}
+          className={styles.image}
+          alt="Uploaded image"
+          title="Uploaded image"
+        />
       </div>
-    );
-  }
+      <div className={styles.text}>
+        <ul className={styles.about}>
+          <li className={styles.description}>
+            <span className={styles.bold}> E-mail: </span>
+            {email}{' '}
+          </li>
+          <li className={styles.description}>
+            <span className={styles.bold}>Date: </span>
+            {date}
+          </li>
+          <li className={styles.description}>
+            <span className={styles.bold}>Charater: </span>
+            {select}
+          </li>
+          <li className={styles.description}>
+            <span className={styles.bold}>Receive news by mail: </span>
+            {checkbox ? 'Yes' : 'No'}
+          </li>
+          <li className={styles.description}>
+            <span className={styles.bold}> Receive advertisements: </span>
+            {switcher ? 'Yes' : 'No'}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
