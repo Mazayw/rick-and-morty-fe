ad2bb6df1e7fa63ccf4269346ac4814061c5e998
Mazayw <mazayw@gmail.com>
2022-10-23 15:50:57 +0300

    feat: not found rewrite

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -1,17 +1,15 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './styles.module.scss';
 
-export default class NotFound extends PureComponent {
-  render() {
-    return (
-      <div className={`${styles.main}`}>
-        <h1 className={styles.title}>404</h1>
-        <h2 className={styles.subtitle}>Page not found</h2>
-        <Link to={'/'} className={styles.link}>
-          Back to main page
-        </Link>
-      </div>
-    );
-  }
+export default function NotFound() {
+  return (
+    <div className={`${styles.main}`}>
+      <h1 className={styles.title}>404</h1>
+      <h2 className={styles.subtitle}>Page not found</h2>
+      <Link to={'/'} className={styles.link}>
+        Back to main page
+      </Link>
+    </div>
+  );
 }
